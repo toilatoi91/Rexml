33d82c608c237f8edbc6686606733723d9b6e3f6
toilatoi91 <nhan.ph2609@sv.sgu.edu.vn>
2015-04-21 08:58:37 +0700

    commit all

---
+++ ReadXML/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ReadXML
+{
+    public class Location
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        public List<Building> Buildings { get; set; }
+    }
+
+    public class Building
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        public List<Room> Rooms { get; set; }
+    }
+
+    public class Room
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+
+    [XmlRoot("info")]
+    public class Info
+    {
+        [XmlArray("locations")]
+        [XmlArrayItem("location")]
+        public List<Location> Locations { get; set; }
+    }
+}
+
+
---
+++ ReadXML/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ReadXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var serializer = new XmlSerializer(typeof(Info));
+            using (var reader = XmlReader.Create("locations.xml"))
+            {
+                Info info = (Info)serializer.Deserialize(reader);
+                List<Location> locations = info.Locations;
+                int a = 1;
+                // do whatever you wanted to do with those locations
+            }
+        }
+    }
+}
